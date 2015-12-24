ee16cffc3ae1667d67e20453cd1b10db88c20487
Spown <spown.05@mail.ru>
2015-12-24 11:32:31 +0100

    ~ m

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-module.exports = function expressPageHandler(mask, handler) {
-	return {'mask': mask, 'handler': handler};
+module.exports = function expressPageHandler() {
+	var ret = {}, middle = []
+	;
+	ret.mask = arguments[0];
+	if (arguments.length > 2) {
+		for (var i = 1; i < arguments.length-1; i++) {
+			middle.push(arguments[i]);
+		}
+	}
+	arguments.length >= 2 && (ret.handler = arguments[arguments.length-1]);
+	return ret;
 };
